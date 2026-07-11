71ac8d468b3b9827c8e6fe867d1bd36ae5576f4d
mja89 <86588358+mja89@users.noreply.github.com>
2026-07-11 18:49:34 +0330

    Update ContactDao.kt

--- app/src/main/java/com/example/callsimulator/data/ContactDao.kt
+++ app/src/main/java/com/example/callsimulator/data/ContactDao.kt
@@ -13,5 +13,12 @@ interface ContactDao {
 
     @Query("DELETE FROM contacts WHERE id = :contactId")
     suspend fun deleteContact(contactId: Int)
-}
 
+    // متد جدید برای دریافت یک مخاطب خاص جهت ویرایش
+    @Query("SELECT * FROM contacts WHERE id = :id")
+    suspend fun getContactById(id: Int): ContactEntity
+
+    // متد جدید برای ذخیره تغییرات (ویرایش)
+    @Update
+    suspend fun update(contact: ContactEntity)
+}
