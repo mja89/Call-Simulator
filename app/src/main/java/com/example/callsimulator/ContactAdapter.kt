5621505bad336c2bcdb5adffcdd6838af786b5e7
mja89 <86588358+mja89@users.noreply.github.com>
2026-07-11 15:08:07 +0330

    Update ContactAdapter.kt

--- app/src/main/java/com/example/callsimulator/ContactAdapter.kt
+++ app/src/main/java/com/example/callsimulator/ContactAdapter.kt
@@ -3,6 +3,7 @@ package com.example.callsimulator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.callsimulator.data.ContactEntity
@@ -11,18 +12,21 @@ class ContactAdapter(private var contacts: List<ContactEntity>) :
     RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
 
     class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val name: TextView = view.findViewById(android.R.id.text1)
+        val image: ImageView = view.findViewById(R.id.ivContactImage)
+        val name: TextView = view.findViewById(R.id.tvName)
+        val phone: TextView = view.findViewById(R.id.tvPhone)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(android.R.layout.simple_list_item_1, parent, false)
+            .inflate(R.layout.item_contact, parent, false)
         return ContactViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
         val contact = contacts[position]
         holder.name.text = contact.name
+        holder.phone.text = contact.phoneNumber
     }
 
     override fun getItemCount() = contacts.size
@@ -30,6 +34,6 @@ class ContactAdapter(private var contacts: List<ContactEntity>) :
     fun updateList(newList: List<ContactEntity>) {
         contacts = newList
         notifyDataSetChanged()
-     }
+    }
     }
     
